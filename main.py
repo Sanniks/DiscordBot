2e2dded07e2f980ca551df60aa07ddaaa4fe63fe
Carlo Mattia Lovecchio <carlo.mattia02@gmail.com>
2026-02-03 19:49:30 +0100

    First Commit. Basic files

---
+++ main.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+handler = logging.FileHandler(filename='discord.log', encoding='etf-8', mode='w')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
